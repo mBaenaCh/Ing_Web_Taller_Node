fc0580765909f9fa97bca91fa623c2435adc6736
mBaenaCh <mateo.baena@hotmail.com>
2021-08-29 00:04:26 -0500

    Application setup

--- customer-service/customer-service/app.js
+++ customer-service/customer-service/app.js
@@ -4,23 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function (req, res) {
+  var pkg = require(path.join(__dirname, 'package.json'));
+  res.json({
+    name: pkg.name,
+    version: pkg.version,
+    status: 'up'
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +33,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: res.locals.message,
+    error: res.locals.error
+  });
 });
 
 module.exports = app;
